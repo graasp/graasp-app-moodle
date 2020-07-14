e7753bd3a4531ed81288868905e4b313cecb46f8
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2020-07-14 10:14:19 +0200

    fix: updated env variable names

--- cypress/integration/basic_workflow.js
+++ cypress/integration/basic_workflow.js
@@ -1,12 +1,12 @@
 // The user MUST be enrolled as teacher in at least 1 course
-const MOODLE_API_ENDPOINT = Cypress.env('moodle')
-  ? Cypress.env('moodle')
+const MOODLE_API_ENDPOINT = Cypress.env('MOODLE_URL')
+  ? Cypress.env('MOODLE_URL')
   : 'http://localhost:8082';
 const MOODLE_USERNAME = 'teacher';
 const MOODLE_PASSWORD = 'teacher';
 
-const GRAASP_URL = Cypress.env('app')
-  ? Cypress.env('app')
+const GRAASP_URL = Cypress.env('APP_URL')
+  ? Cypress.env('APP_URL')
   : 'http://localhost:8000';
 const APP_INSTANCE_ID = '6156e70ab253020033364411';
 const GRAASP_TEACHER_APP_URL = `${GRAASP_URL}/?spaceId=5b56e70ab253020033364411&appInstanceId=${APP_INSTANCE_ID}&mode=teacher&userId=5b56e70ab253020033364416&dev=true`;
